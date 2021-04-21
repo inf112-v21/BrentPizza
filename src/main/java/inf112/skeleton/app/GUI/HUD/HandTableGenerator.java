2f8d6b2d6761588a91a6304022a1f222b83ab009
Steffen.Mo <steffen.mo@student.uib.no>
2021-04-22 00:56:14 +0200

    White background on the priority labels.

--- src/main/java/inf112/skeleton/app/GUI/HUD/HandTableGenerator.java
+++ src/main/java/inf112/skeleton/app/GUI/HUD/HandTableGenerator.java
@@ -1,45 +1,40 @@
 package inf112.skeleton.app.GUI.HUD;
-
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.g2d.NinePatch;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
-
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
-import org.lwjgl.system.CallbackI;
-
+import com.badlogic.gdx.scenes.scene2d.utils.NinePatchDrawable;
 
 import java.util.ArrayList;
 
 public class HandTableGenerator {
 
+    private Label.LabelStyle lStyle;
+    private BitmapFont font;
 
     public Table createHandTable(ArrayList<Button> buttonList, ArrayList<Integer> priority){
         Table table = new Table();
         table.bottom();
 
-        //Perhaps fix later, or delete before we deliver assignement
-        /**
-        Pixmap bgPixmap = new Pixmap(1,1,Pixmap.Format.RGB565);
-        bgPixmap.setColor(Color.WHITE);
-        bgPixmap.fill();
-        //table.setBackground(new TextureRegionDrawable(new TextureRegion(new Texture(bgPixmap))));
-        table.setBounds(0, 0, 1920, 300);
-         **/
+        font = new BitmapFont();
+        font.setColor(Color.BLACK);
+
+        NinePatch patch = new NinePatch(new Texture(Gdx.files.internal("src/main/Resources/white.png")), 10, 10, 10, 10);
+        NinePatchDrawable npd = new NinePatchDrawable(patch);
+        lStyle = new Label.LabelStyle(font,font.getColor());
+        lStyle.background = npd;
+
         table.setFillParent(true);
 
         for (int i = 0; i < buttonList.size(); i++) {
             table.add(buttonList.get(i)).expand(0, 0).pad(10);
-            table.add(new Label(priority.get(i).toString(), new Label.LabelStyle(new BitmapFont(), Color.BLACK)));
+            table.add(new Label(priority.get(i).toString(), new Label.LabelStyle(lStyle)));
         }
         return table;
     }
-
-
-
 
 }
