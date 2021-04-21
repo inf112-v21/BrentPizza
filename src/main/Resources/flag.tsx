85f2bcfaaab9aa83fa932abbafd952b0228638f8
shahez <ssh025@uib.no>
2021-04-21 10:58:47 +0200

    Structuring resource file

--- src/main/Resources/flag.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="flag" tilewidth="150" tileheight="150" tilecount="1" columns="1">
- <image source="flag.png" width="150" height="150"/>
-</tileset>
---
+++ src/main/Resources/map/tileset2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="tileset2" tilewidth="150" tileheight="150" tilecount="144" columns="12">
+ <image source="tileset.png" width="1800" height="1800"/>
+</tileset>
