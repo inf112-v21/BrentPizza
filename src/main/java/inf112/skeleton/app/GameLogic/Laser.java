4573e842b5318ba23ed40715cf4274c161a40378
polsevev <rolf.martin@glomsrud.no>
2021-04-22 16:28:33 +0200

    added lasers

--- src/main/java/inf112/skeleton/app/GameLogic/Laser.java
+++ src/main/java/inf112/skeleton/app/GameLogic/Laser.java
@@ -1,15 +1,11 @@
 package inf112.skeleton.app.GameLogic;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
-
 import com.badlogic.gdx.maps.MapLayer;
 import com.badlogic.gdx.maps.MapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.math.Vector2;
-
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -26,15 +22,16 @@ public class Laser {
 
     public ArrayList<Sprite> createLasers(TiledMap tiledMap){
         MapLayer lasers = tiledMap.getLayers().get(tiledMap.getLayers().getIndex("lazers"));
-
         ArrayList<Sprite> laserSprites = new ArrayList<>();
         for (MapObject laser: lasers.getObjects()) {
             if(laser.getProperties().get("direction").toString().equals("north"))
-                laserSprites.addAll(createLaserVertical("wallNorth", "wallSouth", (float) 60, (float) 150, laser));
+                laserSprites.addAll(createLaserVertical("wallSouth", "wallNorth", (float) 60, (float) 150, laser));
             else if(laser.getProperties().get("direction").toString().equals("south"))
-                laserSprites.addAll(createLaserVertical("wallSouth", "wallNorth", (float) 60, (float) -150, laser));
+                laserSprites.addAll(createLaserVertical("wallNorth", "wallSouth", (float) 60, (float) -150, laser));
             else if(laser.getProperties().get("direction").toString().equals("east"))
-                laserSprites.addAll(createLaserHorizontal("wallEast", "wallWest", (float) -150, (float) 0, laser));
+                laserSprites.addAll(createLaserHorizontal("wallWest", "wallEast", (float) 150, (float) 60, laser));
+            else if(laser.getProperties().get("direction").toString().equals("west"))
+                laserSprites.addAll(createLaserHorizontal("wallEast", "wallWest", (float) -150, (float) 60, laser));
 
         }
         return laserSprites;
@@ -42,12 +39,7 @@ public class Laser {
 
     private ArrayList<Sprite> createLaserVertical(String wallToCheck, String wallToCheck1, Float laserFactorX, Float laserFactorY, MapObject laser){
         ArrayList<Sprite> laserSprites = new ArrayList<>();
-        Integer i;
-        if(laserFactorY == -150){
-            i = 1;
-        }else{
-            i = 0;
-        }
+        Integer i = 0;
 
         while (true){
             Sprite laserrr = new Sprite(texture);
@@ -55,12 +47,11 @@ public class Laser {
             Float laserY = Float.parseFloat(laser.getProperties().get("y").toString()) + i*laserFactorY;
             laserrr.setX(laserX);
             laserrr.setY(laserY);
-
-            if(walls.get(new Vector2(laserX - laserFactorX, laserY)) == (wallToCheck)){
-                laserSprites.add(laserrr);
+            if(walls.get(new Vector2(laserX - laserFactorX, laserY)) == (wallToCheck) && i!=0 || laserY > 1650 || laserY < -26){
                 return laserSprites;
             }
-            else if(walls.get(new Vector2(laserX - laserFactorY, laserY)) == (wallToCheck1) || laserY > 1650 || laserY < -26){
+            if(walls.get(new Vector2(laserX - laserFactorX, laserY)) == (wallToCheck1) ){
+                laserSprites.add(laserrr);
                 return laserSprites;
             }
             for (IPlayer pl: boardLogic.getPlayers()) {
@@ -80,26 +71,21 @@ public class Laser {
 
     private ArrayList<Sprite> createLaserHorizontal(String wallToCheck, String wallToCheck1, Float laserFactorX, Float laserFactorY, MapObject laser){
         ArrayList<Sprite> laserSprites = new ArrayList<>();
-        Integer i;
-        if(laserFactorY == -150){
-            i = 1;
-        }else{
-            i = 0;
-        }
+        Integer i = 0;
+        Float initialY = Float.parseFloat(laser.getProperties().get("y").toString());
+        Float initialX = Float.parseFloat(laser.getProperties().get("x").toString());
         while (true){
             Sprite laserrr = new Sprite(texture);
-            laserrr.rotate(90);
-            Float laserX = Float.parseFloat(laser.getProperties().get("x").toString()) + i*laserFactorX;
-            Float laserY = Float.parseFloat(laser.getProperties().get("y").toString()) + laserFactorY;
-            laserrr.setX(laserX);
+            laserrr.rotate(270);
+            Float laserX = initialX + i*laserFactorX;
+            Float laserY = initialY;
+            laserrr.setX(laserX+laserFactorY);
             laserrr.setY(laserY);
-
-            System.out.println("stuck");
-            if(walls.get(new Vector2(laserX - laserFactorX, laserY)) == (wallToCheck)){
-                laserSprites.add(laserrr);
+            if(walls.get(new Vector2(laserX, laserY)) == (wallToCheck) && i!=0 || laserX > 4200 || laserX < 0){
                 return laserSprites;
             }
-            else if(walls.get(new Vector2(laserX - laserFactorY, laserY)) == (wallToCheck1) || laserX > 4200 || laserX < 0){
+            if(walls.get(new Vector2(laserX, laserY)) == (wallToCheck1)){
+                laserSprites.add(laserrr);
                 return laserSprites;
             }
             for (IPlayer pl: boardLogic.getPlayers()) {
